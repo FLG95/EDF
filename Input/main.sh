b24264ba9ac5afc5f3d6d79a718dc0ded71cb389
paulmrll <paul.morille11@gmail.com>
2024-11-21 10:43:30 +0100

    Update main.sh

--- Input/main.sh
+++ Input/main.sh
@@ -6,19 +6,19 @@ echo "Hello"
 # Chemin du fichier source C
 main="../CodeC/main.c"
 ref="c-wire_v00.dat"
+ref2="c-wire_v25.dat"
 
 # Nom du fichier binaire généré
 binaire="./exe"
-
-cp $ref ../tmp/data
-
-
+mkdir ../tmp/data
+cp $ref ../tmp/data/c-wire_v00_cpy.dat
+cp $ref2 ../tmp/data/c-wire_v25_cpy.dat
 
 gcc "$main" -o "$binaire"
-
 
 ./$binaire
 
 
-rm ../tmp/data
+rm exe
+rm -r ../tmp/*
 
