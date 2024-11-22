b921a88f1d1357a9ba5080f276c1b0e48471c11b
paulmrll <paul.morille11@gmail.com>
2024-11-22 17:24:26 +0100

    Update main.sh

--- Input/main.sh
+++ Input/main.sh
@@ -7,17 +7,18 @@ echo "Hello"
 main="../CodeC/main.c"
 ref="c-wire_v00.dat"
 ref2="c-wire_v25.dat"
+dir_makefile="../CodeC"
+
 
 # Nom du fichier binaire généré
-binaire="./exe"
 mkdir ../tmp/data
 cp $ref ../tmp/data/c-wire_v00_cpy.dat
 cp $ref2 ../tmp/data/c-wire_v25_cpy.dat
 
-gcc "$main" -o "$binaire"
 
-./$binaire
+cd $dir_makefile
 
+make all
 
 rm exe
 rm -r ../tmp/*
