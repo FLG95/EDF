c5cd7eae5ea867891e39638bfb347c762f43cde1
paulmrll <paul.morille11@gmail.com>
2024-11-22 22:32:08 +0100

    Update creerAvl.c

--- CodeC/creerAvl.c
+++ CodeC/creerAvl.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
-#include "creer_avl.h"
 #include <stdbool.h>
 #include <stdlib.h>
+
+#include "creer_avl.h"
 
 
 
@@ -114,63 +115,7 @@ arbre* inserer(arbre* tete, int a, int* h) {
 }
 
 // Parcours en largeur
-file* creerFile(arbre* a) {
-    file* new = malloc(sizeof(file));
-    if (new == NULL) {
-        fprintf(stderr, "Erreur: allocation mémoire échouée pour la file.\n");
-        exit(EXIT_FAILURE);
-    }
-    new->actuel = a;
-    new->suivant = NULL;
-    return new;
-}
 
-file* enfiler(file* f, arbre* tete) {
-    file* new = creerFile(tete);
-    if (f == NULL) {
-        return new;
-    }
-    file* tmp = f;
-    while (tmp->suivant != NULL) {
-        tmp = tmp->suivant;
-    }
-    tmp->suivant = new;
-    return f;
-}
-
-arbre* defiler(file** f) {
-    if (*f == NULL) return NULL;
-
-    file* tmp = *f;
-    arbre* courant = tmp->actuel;
-    *f = tmp->suivant;
-    free(tmp);
-    return courant;
-}
-
-void parcoursLargeur(arbre* tete) {
-    if (tete == NULL) return;
-
-    file* f = enfiler(NULL, tete);
-    while (f != NULL) {
-        arbre* courant = defiler(&f);
-        if (courant != NULL) {
-            printf("%d ", courant->nb);
-            if (courant->fg != NULL) f = enfiler(f, courant->fg);
-            if (courant->fd != NULL) f = enfiler(f, courant->fd);
-        }
-    }
-    printf("\n");
-}
-
-// Affichage en profondeur
-void afficher(arbre* tete) {
-    if (tete != NULL) {
-        afficher(tete->fg);
-        printf("%d ", tete->nb);
-        afficher(tete->fd);
-    }
-}
 
 int hauteur(arbre* tete) {
     if (tete == NULL) return 0;
