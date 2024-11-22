e5ffdc2ddb5d400ae11736601f0146c34ec5d5ea
paulmrll <paul.morille11@gmail.com>
2024-11-22 22:48:14 +0100

    Update main.sh

--- Input/main.sh
+++ Input/main.sh
@@ -9,6 +9,14 @@ ref="c-wire_v00.dat"
 ref2="c-wire_v25.dat"
 dir_makefile="../CodeC"
 dir_origine="../Input"
+dir_tmp="../tmp"
+
+#vérifie si le dossier tmp existe
+if [ -d $dir_tmp ]; then
+  rm $dir_tmp/*
+else
+  mkdir $dir_tmp;
+fi
 
 
 # Nom du fichier binaire généré
@@ -16,13 +24,15 @@ mkdir ../tmp/data
 cp $ref ../tmp/data/c-wire_v00_cpy.dat
 cp $ref2 ../tmp/data/c-wire_v25_cpy.dat
 
-
+#lancement du code C avec le Makefile
 cd $dir_makefile
 
 make all
 make clean
 
 cd $dir_origine
+
+#suppression des fichiers et/ou dossiers du dossiers tmp
 rm -r ../tmp/*
 
 
