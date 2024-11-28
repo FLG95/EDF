411b0ec41ad21b853a46022240e058cfb70f342f
paulmrll <paul.morille11@gmail.com>
2024-11-29 00:54:12 +0100

    Update type_avl.h

--- CodeC/include/type_avl.h
+++ CodeC/include/type_avl.h
@@ -1,6 +1,15 @@
 #ifndef TYPE_AVL
 #define TYPE_AVL
-
+typedef struct donnees{
+    int Powerplant;
+    int hv_b;
+    int hv_a;
+    int lv;
+    int entreprise;
+    int particuliers;
+    int consommation;
+    int production;
+} donnees;
 typedef struct arbre {
     int nb;
     struct arbre* fd;
@@ -8,8 +17,14 @@ typedef struct arbre {
     int equilibre;
 } arbre;
 typedef struct file {
+    donnees a;
     arbre* actuel;
     struct file* suivant;
 } file;
+typedef struct file2 {
+    donnees a;
+    struct file** actuel;
+    struct file* suivant;
+} file2;
 
 #endif //TYPE_AVL
