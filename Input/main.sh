7cea6a3818b1a2633eef4110e65314644bc958b1
paulmrll <paul.morille11@gmail.com>
2024-11-28 16:59:55 +0100

    Update main.sh

--- Input/main.sh
+++ Input/main.sh
@@ -12,7 +12,7 @@ dir_tmp="../tmp"
 
 #vérifie si le dossier tmp existe
 if [ -d $dir_tmp ]; then
-  rm $dir_tmp/*
+  rm -r $dir_tmp/*
 else
   mkdir $dir_tmp;
 fi
@@ -30,13 +30,14 @@ fi
 cp $1 ../tmp/data/c-wire_v00_cpy.dat
 
 #extrait les données utiles dans un fichier temporaire dans le dossier temporaire
-awk -F ';' '$4 == 1 || $4 == 2' c-wire_v00.dat > ../tmp/temp.txt
+awk -F ';' '$4 == 2' c-wire_v00.dat > ../tmp/temp.txt
 
 #lancement du code C avec le Makefile en passant le fichier en paramètre
 cd $dir_makefile || exit
-make all FILE=../Input/c-wire_v00.dat
+make all FILE=../tmp/temp.txt
 make clean
 cd $dir_origine || exit
 
 #suppression des fichiers et/ou dossiers du dossiers tmp
-rmdir ../tmp/
+rm -r ../tmp/data/
+rm ../tmp/*
