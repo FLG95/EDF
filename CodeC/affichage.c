ba0860a45e026d742c888827b16d2020e3337a81
paulmrll <paul.morille11@gmail.com>
2024-11-29 10:23:16 +0100

    Update affichage.c

--- CodeC/affichage.c
+++ CodeC/affichage.c
@@ -5,6 +5,7 @@
 
 void traiter(arbre* tete){
     if (tete != NULL){
-        printf("%d\n", tete->nb);
+        printf("%d %d %d %d %d %d %d %d\n",
+               tete->a.Powerplant, tete->a.hv_b, tete->a.hv_a, tete->a.lv, tete->a.entreprise, tete->a.particuliers, tete->a.production, tete->a.consommation);
     }
 }
