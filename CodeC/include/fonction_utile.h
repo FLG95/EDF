e18c239894cbca55afdef7410cbc986df69345d0
paulmrll <paul.morille11@gmail.com>
2024-12-05 09:15:14 +0100

    Update fonction_utile.h

--- CodeC/include/fonction_utile.h
+++ CodeC/include/fonction_utile.h
@@ -11,6 +11,7 @@ int max(int a, int b);
 //fonction pour libérer la mémoire de l'arbre
 void freeAvl(arbre* tete);
 int comparaisonData(donnees a, donnees b);
+long long int somme(long unsigned a, long long int b);
 
 #endif //FONCTION_UTILE
 
