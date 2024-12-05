7805d6e4d91316911d0501ac98a2ac8ea1ba455b
paulmrll <paul.morille11@gmail.com>
2024-12-05 10:45:21 +0100

    Update parcours.h

--- CodeC/include/parcours.h
+++ CodeC/include/parcours.h
@@ -13,7 +13,5 @@ file* enfiler(file* f, arbre* tete);
 arbre* defiler(file** f);
 
 void parcoursLargeur(arbre* tete);
-void parcoursInfixe(arbre* tete, long long int* b);
-void recuperer10inf(int* compteur, arbre** tab, arbre* tete);
-void recuperer10sup(int* compteur, arbre** tab, arbre* tete);
+void parcoursInfixe(arbre* tete);
 #endif //PARCOURS_H
