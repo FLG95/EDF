fa35f5dc96b51415b2e267d1a84d3fc636c679b6
paulmrll <paul.morille11@gmail.com>
2024-12-05 10:59:42 +0100

    Update fonctionUtile.c

--- CodeC/fonctionUtile.c
+++ CodeC/fonctionUtile.c
@@ -22,22 +22,40 @@ void freeAvl(arbre* tete) {
 }
 
 int comparaisonData(donnees a, donnees b){
-    if (a.Powerplant != b.Powerplant){
-        return a.Powerplant - b.Powerplant;
-    } else if (a.hv_b != b.hv_b){
+   if (a.hv_b != b.hv_b){
         return a.hv_b - b.hv_b;
     } else if (a.hv_a != b.hv_a){
         return a.hv_a - b.hv_a;
     } else if (a.lv != b.lv){
         return a.lv - b.lv;
-    } else if (a.entreprise != b.entreprise){
-        return a.entreprise - b.entreprise;
-    } else if (a.particuliers != b.particuliers){
-        return a.particuliers - b.particuliers;
     }
     return 0;
 }
 
 long long int somme(long unsigned a, long long int b){
     return a + b;
+}
+int estLv(donnees a){
+    if (a.lv != 0){
+        return 1;
+    } else {
+        return 0;
+    }
+    return 0;
+}
+int estHvb(donnees a){
+    if (a.lv == 0 && a.hv_b != 0 && a.hv_a == 0){
+        return 1;
+    } else {
+        return 0;
+    }
+    return 0;
+}
+int estHva(donnees a){
+    if (a.lv == 0 && a.hv_b != 0 && a.hv_a != 0){
+        return 1;
+    } else {
+        return 0;
+    }
+    return 0;
 }
