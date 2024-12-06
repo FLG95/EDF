3a58c4e3005d904fe6da81d10c820d013f48f825
paulmrll <paul.morille11@gmail.com>
2024-12-06 15:55:42 +0100

    Update outFile.h

--- CodeC/include/outFile.h
+++ CodeC/include/outFile.h
@@ -5,9 +5,12 @@
 #include <stdlib.h>
 
 #include "type_avl.h"
+#include "fonction_utile.h"
 
 void write10sup(arbre* tete, int* h, FILE* fichier);
 void write10less(arbre* tete, int* h, FILE* fichier);
-void centerWrite(arbre* tete);
+void centerWrite10(arbre* tete);
+void writeAllData(arbre* tete, FILE* fichier, int* h, char* arg1, char* arg2);
+void centerWrite(arbre* tete, char* arg2, char* arg3);
 
 #endif //OutFile
