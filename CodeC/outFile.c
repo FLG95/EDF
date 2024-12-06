f27bf7c2abb6ece0c82f01aeddcdc01f21012afa
paulmrll <paul.morille11@gmail.com>
2024-12-06 17:20:29 +0100

    Update outFile.c

--- CodeC/outFile.c
+++ CodeC/outFile.c
@@ -1,41 +1,36 @@
 #include "include/outFile.h"
-void write10sup(arbre* tete, int* h, FILE* fichier){
+void write10sup(arbre* tete, int* h, FILE* fichier, char* arg1, char* arg2){
 
     if (tete != NULL && *h < 10) {
-        write10sup(tete->fd, h, fichier);
+        write10sup(tete->fd, h, fichier, arg1, arg2);
         if (*h == 0){
-            fprintf(fichier, "PowerPlant:hvb:hva:lv:entreprise:particulier:production:consommation\n");
+            fprintf(fichier, "Station %s : Capacité : Consommation %s\n", arg1, arg2);
         }
         if (*h < 10) {
             *h = *h + 1;
-            fprintf(fichier, "%lu:%lu:%lu:%lu:%lu:%lu:%lu:%lu\n",
-                    tete->a.Powerplant, tete->a.hv_b, tete->a.hv_a,
-                    tete->a.lv, tete->a.entreprise, tete->a.particuliers,
-                    tete->a.production, tete->a.consommation);
+            fprintf(fichier, "%d:%lu:%lu\n",
+                    tete->a.id, tete->a.production, tete->a.consommation);
         }
-        write10sup(tete->fg, h, fichier);
+        write10sup(tete->fg, h, fichier, arg1, arg2);
     }
 }
-void write10less(arbre* tete, int* h, FILE* fichier){
+void write10less(arbre* tete, int* h, FILE* fichier, char* arg1, char* arg2){
 
     if (tete != NULL && *h < 10) {
-        // Parcours du sous-arbre gauche
-        write10less(tete->fg, h, fichier);
+        write10less(tete->fg, h, fichier, arg1, arg2);
         if (*h == 0){
-            fprintf(fichier, "PowerPlant:hvb:hva:lv:entreprise:particulier:production:consommation\n");
+            fprintf(fichier, "Station %s : Capacité : Consommation %s\n", arg1, arg2);
         }
         if (*h < 10) {
             *h = *h + 1;
-            fprintf(fichier, "%lu:%lu:%lu:%lu:%lu:%lu:%lu:%lu\n",
-                    tete->a.Powerplant, tete->a.hv_b, tete->a.hv_a,
-                    tete->a.lv, tete->a.entreprise, tete->a.particuliers,
-                    tete->a.production, tete->a.consommation);
+            fprintf(fichier, "%d:%lu:%lu\n",
+                    tete->a.id, tete->a.production, tete->a.consommation);
         }
-        write10less(tete->fd, h, fichier);
+        write10less(tete->fd, h, fichier, arg1, arg2);
     }
 }
 
-void centerWrite10(arbre* tete){
+void centerWrite10(arbre* tete, char* arg1, char* arg2){
     if (tete == NULL){
         exit(1);
     }
@@ -46,8 +41,8 @@ void centerWrite10(arbre* tete){
     if (fichier_inf == NULL || fichier_sup == NULL){
         exit(1);
     }
-    write10sup(tete ,&h_sup, fichier_sup);
-    write10less(tete, &h_inf, fichier_inf);
+    write10sup(tete ,&h_sup, fichier_sup, arg1, arg2);
+    write10less(tete, &h_inf, fichier_inf, arg1, arg2);
     fclose(fichier_sup);
     fclose(fichier_inf);
 }
