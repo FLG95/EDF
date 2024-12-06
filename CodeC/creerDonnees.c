8946a8db00e0ae77969f63ea88d2555bc6fdbb1f
paulmrll <paul.morille11@gmail.com>
2024-12-06 12:42:53 +0100

    Update creerDonnees.c

--- CodeC/creerDonnees.c
+++ CodeC/creerDonnees.c
@@ -1,5 +1,6 @@
 #include "include/type_avl.h"
 #include "include/creer_avl.h"
+#include "include/fonction_utile.h"
 
 FILE* transformerFichier(FILE* fichier){
     if (fichier == NULL){
@@ -31,27 +32,30 @@ FILE* transformerFichier(FILE* fichier){
     return out;
 }
 
-arbre* ensembleDonne(FILE* fichier, int* h){
-    if (fichier == NULL){
+void ensembleDonne(FILE* fichier, int* hStation, int* hConso, arbre** stationTree, arbre** consoTree, char* type) {
+    if (fichier == NULL) {
         exit(1);
     }
-    FILE* out = transformerFichier(fichier);
-
-
+    FILE *out = transformerFichier(fichier);
+    donnees* tmp = NULL;
+    tmp = init();
+    if (tmp == NULL){
+        exit(1);
+    }
     char ligne2[256];
     donnees b;
-    arbre* tete = NULL;
+    arbre *tete = NULL;
     int i = 0;
     fgets(ligne2, sizeof(ligne2), out);
     while (fgets(ligne2, sizeof(ligne2), out) != NULL) {
-        printf("%s", ligne2);
+        //printf("%s", ligne2);
         sscanf(ligne2, "%lu;%lu;%lu;%lu;%lu;%lu;%lu;%lu;",
                &b.Powerplant, &b.hv_b, &b.hv_a, &b.lv, &b.entreprise, &b.particuliers, &b.production, &b.consommation);
-        tete = inserer(tete, b, h);
+
+        addTree(stationTree, consoTree, b, hStation, hConso, type, tmp);
         printf("%d\n", i);
         i++;
     }
     printf("\n\n\n\n");
-    fclose (out);
-    return tete;
+    fclose(out);
 }
