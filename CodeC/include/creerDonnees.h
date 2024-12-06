77d1ccd0f5d00b324864388dcfdb5c7dfca65b1a
paulmrll <paul.morille11@gmail.com>
2024-12-06 12:42:47 +0100

    Update creerDonnees.h

--- CodeC/include/creerDonnees.h
+++ CodeC/include/creerDonnees.h
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 
 #include "type_avl.h"
+#include "fonction_utile.h"
 
 FILE* transformerFichier(FILE* fichier);
-arbre* ensembleDonne(FILE* fichier, int* h);
+void ensembleDonne(FILE* fichier, int* hStation, int* hConso, arbre** stationTree, arbre** consoTree, char* type);
 
 
 #endif //CreerDonnees
