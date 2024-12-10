eb387c6ac4f6fac8f9752eb59d02f673479fe298
paulmrll <paul.morille11@gmail.com>
2024-12-10 18:16:32 +0100

    Create create_avl.h

---
+++ CodeC/include/create_avl.h
@@ -0,0 +1,32 @@
+#ifndef create_AVL_H
+#define create_AVL_H
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "type_avl.h"
+#include "parcours.h"
+#include "fonction_utile.h"
+
+
+
+
+int min(int a, int b);
+int max(int a, int b);
+tree* create(Data a);
+tree* leftRotation(tree* head);
+tree* RightRotation(tree* head);
+tree* doubleLeftRotation(tree* head);
+tree* doubleRightRotation(tree* head);
+tree* balancing(tree* head);
+tree* insertStation(tree* head, Data a, int* h);
+tree* insererConso(tree* head, Data a, int* h);
+void freeAvl(tree* head);
+int height(tree* head);
+bool isAVL(tree* head);
+void addTree(tree** stationTree, tree** consoTree, Data b, int* hStation, int* hConso, char* station, Data* tmp, int* i);
+tree* sortByProduction(tree* out, Data tmp, int* h);
+tree* sortByAbs(tree* out, Data tmp, int* h);
+
+#endif //create_AVL
