c14bf580f867556c510766259132aff283d04767
paulmrll <paul.morille11@gmail.com>
2024-12-10 22:20:57 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -158,10 +158,10 @@ case $2 in
       awk -F ';' 'NR == 1 || ($4 != "-")' $1 > tmp/data.txt
 
   elif [ $3 == "indiv" ]; then
-      awk -F ';' 'NR == 1 || ($4 != "-" && $6 != "-")' $1 > tmp/data.txt
+      awk -F ';' 'NR == 1 || ($4 != "-" && $6 != "-") || ($4 !="-" && $5 == "-")' $1 > tmp/data.txt
 
   elif [ $3 == "comp" ]; then
-      awk -F ';' 'NR == 1 || ($4 != "-" && $5 != "-")' $1 > tmp/data.txt
+      awk -F ';' 'NR == 1 || ($4 != "-" && $5 != "-") || ($4 != "-" && $6 == "-")' $1 > tmp/data.txt
   else
       echo "erreur : l'un des arguments spécifiés est incorrect"
       exit 0
@@ -207,6 +207,6 @@ END_TIME=$(date +%s)
 PROCESSUS_TIME=$((END_TIME - START_TIME))
 echo "Le programme a mit : $PROCESSUS_TIME secondes"
 
-#rm tmp/*
+rm tmp/*
 
 echo "$ascii3"
