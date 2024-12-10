9582d7e68b06fb63b8fed980d21a166c94fc8585
paulmrll <paul.morille11@gmail.com>
2024-12-10 18:16:45 +0100

    Update fonction_utile.h

--- CodeC/include/fonction_utile.h
+++ CodeC/include/fonction_utile.h
@@ -6,19 +6,19 @@
 #include <string.h>
 
 #include "type_avl.h"
-#include "creer_avl.h"
+#include "create_avl.h"
 
 int min(int a, int b);
 int max(int a, int b);
-//fonction pour libérer la mémoire de l'arbre
-void freeAvl(arbre* tete);
-int comparaisonData(donnees a, donnees b);
+//fonction pour libérer la mémoire de l'tree
+void freeAvl(tree* head);
+int comparaisonData(Data a, Data b);
 long long int somme(long unsigned a, long long int b);
-int estLv(donnees a);
-int estHvb(donnees a);
-int estHva(donnees a);
-void nbNoeud(long long unsigned* i, arbre* tete);
-donnees* init();
+int isLv(Data a);
+int isHvb(Data a);
+int isHva(Data a);
+void nbNodes(long long unsigned* i, tree* head);
+Data* init();
 char* addCharToChar(char* origin, char* toAdd);
 
 int absoluteValue(long unsigned i, long unsigned j);
