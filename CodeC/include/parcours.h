48b861854c1188a926c45a8e62aed52628c47bb7
paulmrll <paul.morille11@gmail.com>
2024-12-11 14:57:10 +0100

    Update parcours.h

--- CodeC/include/parcours.h
+++ CodeC/include/parcours.h
@@ -3,17 +3,8 @@
 #include <stdio.h>
 
 #include "type_avl.h"
-#include "affichage.h"
 #include "fonction_utile.h"
 
-
-
-file* createFile(tree* a);
-file* enfiler(file* f, tree* head);
-tree* defiler(file** f);
-
-void parcoursLargeur(tree* head);
-void parcoursInfixe(tree* head);
 void parcoursRefresh(tree** head, Data a);
 tree* parcoursSortProduction(tree* head, tree* tmp, int* h);
 tree* parcoursSortAbs(tree* head, tree* tmp, int* h);
