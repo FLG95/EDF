d6c009526498be479dce7868495a9dd7a0e39f53
paulmrll <paul.morille11@gmail.com>
2024-12-11 20:40:52 +0100

    Update createData.c

--- CodeC/createData.c
+++ CodeC/createData.c
@@ -2,12 +2,14 @@
 #include "include/create_avl.h"
 #include "include/fonction_utile.h"
 
+
+//Transform the "-" of the file by "0"
 FILE* transformerFichier(FILE* fichier){
     if (fichier == NULL){
         exit(1);
     }
-    FILE* out = fopen("../tmp/tmp.txt", "w");
-    if (out == NULL){
+    FILE* new_file = fopen("../tmp/tmp.txt", "w");
+    if (new_file == NULL){
         exit(1);
     }
     char line[256];
@@ -19,22 +21,23 @@ FILE* transformerFichier(FILE* fichier){
             }
         }
         // Écrire la line modifiée dans le fichier de sortie.
-        fputs(line, out);
+        fputs(line, new_file);
     }
     fclose (fichier);
-    fclose (out);
-    out = fopen("../tmp/tmp.txt", "r");
-    if (out == NULL){
+    fclose (new_file);
+    new_file = fopen("../tmp/tmp.txt", "r");
+    if (new_file == NULL){
         exit(1);
     }
-    return out;
+    return new_file;
 }
 
-void ensembleDonne(FILE* fichier, int* hStation, int* hConso, tree** stationTree, tree** consoTree, char* type) {
+//Take data of a File and add it to a tree
+void ensembleDonne(FILE* fichier, int* hStation, tree** stationTree, char* type) {
     if (fichier == NULL) {
         exit(1);
     }
-    FILE *out = transformerFichier(fichier);
+    FILE *new_file = transformerFichier(fichier);
     char line2[256];
     Data* tmp = NULL;
     tmp = init();
@@ -43,16 +46,16 @@ void ensembleDonne(FILE* fichier, int* hStation, int* hConso, tree** stationTree
     }
     int i = 0;
     sscanf(line2, "%lu;%lu;%lu;%lu;%lu;%lu;%lu;%lu;",
-           &tmp->Powerplant, &tmp->hv_b, &tmp->hv_a, &tmp->lv, &tmp->entreprise, &tmp->particuliers, &tmp->production, &tmp->consommation);
-    addTree(stationTree, consoTree, *tmp, hStation, hConso, type, tmp,  &i);
+           &tmp->Powerplant, &tmp->hv_b, &tmp->hv_a, &tmp->lv, &tmp->entreprise, &tmp->particuliers, &tmp->production, &tmp->consumption);
+    addTree(stationTree, *tmp, hStation, type, tmp,  &i);
     Data b;
     tree *head = NULL;
-    fgets(line2, sizeof(line2), out);
-    while (fgets(line2, sizeof(line2), out) != NULL) {
+    fgets(line2, sizeof(line2), new_file);
+    while (fgets(line2, sizeof(line2), new_file) != NULL) {
         sscanf(line2, "%lu;%lu;%lu;%lu;%lu;%lu;%lu;%lu;",
-               &b.Powerplant, &b.hv_b, &b.hv_a, &b.lv, &b.entreprise, &b.particuliers, &b.production, &b.consommation);
-        addTree(stationTree, consoTree, b, hStation, hConso, type, tmp, &i);
+               &b.Powerplant, &b.hv_b, &b.hv_a, &b.lv, &b.entreprise, &b.particuliers, &b.production, &b.consumption);
+        addTree(stationTree, b, hStation, type, tmp, &i);
     }
     printf("\n\n\n\n");
-    fclose(out);
+    fclose(new_file);
 }
