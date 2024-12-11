fbfa154178e72d6f3b07b137fe608759e5df9a6a
paulmrll <paul.morille11@gmail.com>
2024-12-11 14:56:56 +0100

    Delete affichage.c

--- CodeC/affichage.c
+++
@@ -1,8 +0,0 @@
-#include "include/affichage.h"
-
-void traiter(tree* head){
-    if (head != NULL){
-        printf("%lu %lu %lu %lu %lu %lu %lu %lu\n",
-               head->a.Powerplant, head->a.hv_b, head->a.hv_a, head->a.lv, head->a.entreprise, head->a.particuliers, head->a.production, head->a.consommation);
-    }
-}
